f272e2dc131584054379ef571a5b66f5e6988b71
dhordyko <you@example.comdhordyko@gmail.com>
2022-06-02 14:21:00 +0200

    add bootstrap

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,4 +1,2 @@
+
 const sum = require("./module/sum.js");
-console.log(sum(10,5));
-console.log(sum(10,7));
-console.log(sum(10,9));
--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,4 +1,4 @@
-const { src, dest } = require("gulp");
+const { src, dest, parallel } = require("gulp");
 const plumber = require("gulp-plumber");
 const path = require("../config/path");
 const modul = require("../config/module");
@@ -6,7 +6,7 @@ const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const webpack = require("webpack-stream");
 const js = () => {
-    return src(path.js.src)
+    return src([path.js.src])
         .pipe(plumber(modul.error))
         .pipe(babel())
         .pipe(webpack({
@@ -14,5 +14,13 @@ const js = () => {
         }))
         .pipe(uglify())
         .pipe(dest(path.js.dest))
+        
 }
-module.exports = js
+const jsVendor=()=> {
+    return src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      ])
+      .pipe(dest(path.js.dest))
+  }
+module.exports = parallel(js,jsVendor);
